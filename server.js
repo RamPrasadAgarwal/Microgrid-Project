bbe6df049120217efa817ca6b7b2c90add0e6a92
Ram Prasad Agarwal <ramprasadagarwal313@gmail.com>
2017-05-03 04:19:46 +0530

    server files changed

--- server.js
+++ server.js
@@ -1,15 +1,7 @@
-var serverstatic = require('serve-static');
+var http = require('http');
 var express = require('express');
 var app = express();
 
-require('./app/route')(app);
+require('./route')(app);
 
-var server = app.listen(80, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-	app.use(express.static('public'));
-
-  console.log('Example app listening at http://%s:%s', host, port);
-
-});
+var server = http.createServer(app).listen(process.env.PORT || 80);
