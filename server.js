bad9ae9cb71a68c259bdf4fc62d5cd85d94d8260
Ram Prasad Agarwal <ramprasadagarwal313@gmail.com>
2017-05-03 04:22:13 +0530

    server files changed

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var http = require('http');
 var express = require('express');
 var app = express();
 
-require('./route')(app);
+require('./app/route')(app);
+app.use(express.static('public'));
 
 var server = http.createServer(app).listen(process.env.PORT || 80);
